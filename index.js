c558ea94c6b2e22bd08d75945d743c9020aad521
Snugglez <32309975+Snugglez@users.noreply.github.com>
2018-08-21 10:34:33 -0400

    damn it caali

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const Command = require('Command'),
-		GameState = require('tera-game-state')
-module.exports = function unlein(dispatch) {
-const c = Command(dispatch),
-		g = GameState(dispatch);
+module.exports = function unlein(d) {
 let enabled = true;
 let rejuvId = 149644
-c.add('shitzerker', () => {
+d.command.add('shitzerker', () => {
 enabled = !enabled
-c.message(`unlein is now ${enabled ? 'enabled' : 'disabled'}.`)})
-c.add('setpot', (chat) => {
+d.command.message(`unlein is now ${enabled ? 'enabled' : 'disabled'}.`)})
+d.command.add('setpot', (chat) => {
 var regexId = /#(\d*)@/;
 var id = chat.match(regexId);
 if (id){rejuvId = parseInt(id[1])}
-c.message(`potion set to ` + rejuvId)})
-dispatch.hook('S_ABNORMALITY_BEGIN', 2, (event) => {
+d.command.message(`potion set to ` + rejuvId)})
+d.hook('S_ABNORMALITY_BEGIN', 2, (event) => {
 if(!enabled) return;
-if(event.id === 401705 && g.me.is(event.target))
-dispatch.toServer('C_USE_ITEM', 3, {
-gameId: g.me.gameId,
+if(event.id === 401705 && d.game.me.is(event.target))
+d.toServer('C_USE_ITEM', 3, {
+gameId: d.game.me.gameId,
 id: rejuvId,
 amount: 1,
 unk4: true
